9f5d9a2b5f55b3ad26cda8c6691522f3c78a3cb5
joelriveradev <joelriveradev@gmail.com>
2024-03-06 11:12:52 -0600

    feat: create uuids

--- app/utils.ts
+++ app/utils.ts
@@ -10,7 +10,11 @@ export function generateUniqueId() {
   let id = ''
 
   if (typeof window !== 'undefined') {
-    id = Date.now().toString(36) + Math.random().toString(36).substring(2)
+    id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = (Math.random() * 16) | 0
+      const v = c === 'x' ? r : (r & 0x3) | 0x8
+      return v.toString(16)
+    })
   }
   return id
 }
