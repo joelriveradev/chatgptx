667600e686cae0a9528222afe179380709c9ee95
joelriveradev <joelriveradev@gmail.com>
2024-03-06 20:45:45 -0600

    feat: make suggestions scrollable on mobile complete with snapping

--- app/components/custom/chat-suggestions.tsx
+++ app/components/custom/chat-suggestions.tsx
@@ -32,14 +32,14 @@ export const ChatSuggestions = ({ onSuggestionClick }: Props) => {
     if (suggestions) {
       animate(
         suggestions,
-        { y: -18 },
+        { y: -3, opacity: 1 },
         { delay: stagger(0.1), duration: 0.5, easing: spring() }
       )
     }
   }, [])
 
   return (
-    <ul className='grid w-full grid-flow-row grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-3'>
+    <ul className='flex items-center h-24 lg:h-44 snap-x snap-mandatory overflow-x-auto scroll-smooth lg:grid w-full lg:grid-flow-row grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-3 lg:gap-y-px'>
       {suggestions.map((suggestion, i) => {
         const { title, description } = suggestion
 
@@ -48,10 +48,11 @@ export const ChatSuggestions = ({ onSuggestionClick }: Props) => {
             id='suggestion'
             key={`${title}-${i}`}
             onClick={() => onSuggestionClick(suggestion)}
+            className='opacity-0 transform translate-y-40 snap-center'
           >
             <button
               type='button'
-              className='flex flex-col justify-start w-full text-left h-full bg-transparent border border-zinc-800 rounded-xl p-4 hover:bg-zinc-900 cursor-pointer'
+              className='w-[300px] flex flex-col shrink-0 justify-start lg:w-full text-left h-full bg-transparent border border-zinc-800 rounded-xl p-4 hover:bg-zinc-900 cursor-pointer'
             >
               <p className='font-semibold text-white antialiased text-sm mb-1'>{title}</p>
               <p className='text-zinc-600 text-sm'>{description}</p>
