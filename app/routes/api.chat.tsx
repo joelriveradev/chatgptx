87e91b767171396d40611ffee46c4d188cf65f27
joelriveradev <joelriveradev@gmail.com>
2024-03-29 16:28:41 -0500

    fix: patch for streaming text response

--- app/routes/api.chat.tsx
+++ app/routes/api.chat.tsx
@@ -1,12 +1,16 @@
 import { json, type ActionFunctionArgs } from '@vercel/remix'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { OpenAIStream } from 'ai'
+import { StreamingTextResponse } from '~/lib/polyfills'
 import { openai } from '~/lib/openai.server'
+import { TransformStream } from 'web-streams-polyfill'
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   if (request.method !== 'POST') {
     return json({ message: 'Method not allowed' }, 405)
   }
   const { messages, model } = await request.json()
+
+  globalThis.TransformStream = TransformStream
 
   const response = await openai.chat.completions.create({
     model,
